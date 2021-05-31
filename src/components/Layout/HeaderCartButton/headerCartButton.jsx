8567d4650b8cd3acbc37388b599a15aaf228c05e
v-prsingh <v-prsingh@LP-5CG0250FFG>
2021-05-31 06:28:07 +0530

    Animation

--- src/components/Layout/HeaderCartButton/headerCartButton.jsx
+++ src/components/Layout/HeaderCartButton/headerCartButton.jsx
@@ -1,17 +1,34 @@
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../../Cart/CartIcon";
 import CartContext from "../../../store/cart-context";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 const HeaderCartButton = (props) => {
+  const [isPop, setIsPop] = useState(false);
   const { cartState, setCartState } = useContext(CartContext);
-  const reducer = (accumulator, currentValue) => accumulator + currentValue.amount;
-   var total = 0;
-   var array = [...cartState.items];
-   total = array.reduce(reducer,0);
-   console.log(array);
+  const { items } = cartState;
+  const reducer = (accumulator, currentValue) =>
+    accumulator + currentValue.amount;
+  var total = 0;
+  var array = [...cartState.items];
+  total = array.reduce(reducer, 0);
+  console.log(array);
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setIsPop(true);
+    var id = setTimeout(() => {
+      setIsPop(false);
+    }, 300);
+    return()=>{clearTimeout(id)};
+  }, [total]);
 
   return (
-    <button className={classes.button} onClick={props.showCartHandler}>
+    <button
+      className={`${classes.button} ${isPop ? classes.bump : " "} `}
+      onClick={props.showCartHandler}
+    >
       <span className={classes.icon}>
         <CartIcon />
       </span>
